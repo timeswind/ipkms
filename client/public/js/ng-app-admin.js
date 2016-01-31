9c05f392399be65097fb1a3579919ab6ce7091c5
Timeswind Yang <tswymt@gmail.com>
2016-01-31 00:38:42 +0000

    first commit test

---
+++ client/public/js/ng-app-admin.js
@@ -0,0 +1,164 @@
+angular.module('admin', [])
+  .config(function($interpolateProvider, $httpProvider){
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+  //Reset headers to avoid OPTIONS request (aka preflight)
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+})
+  .controller('manageTeachers', function($rootScope, $scope, $http) {
+  getTeachers();
+
+  function getTeachers(){
+    $http({
+      method: 'GET',
+      url: '../api/teachers'
+    }).then(function successCallback(response) {
+
+      $scope.teachers = response.data;
+      console.log(response.data);
+
+    }, function errorCallback(response) {
+      console.log("error to get user, maybe not login");
+    });
+  }
+
+  $scope.deleteTeacher = function(userId, teacherId){
+    $http({
+      method: 'DELETE',
+      url: '../api/teacher/' + userId + "/" + teacherId
+    }).then(function successCallback(response) {
+      getTeachers();
+      $rootScope.$broadcast('refreshUserList');
+
+      console.log(response);
+
+    }, function errorCallback(response) {
+      console.log(response);
+    })
+  }
+
+  $scope.$on('refreshTeacherList', function(event, args) {
+    getTeachers();
+  });
+
+})
+  .controller('manageUsers', function($rootScope, $scope, $http) {
+  getUsers();
+
+  function getUsers(){
+    $http({
+      method: 'GET',
+      url: '../api/users'
+    }).then(function successCallback(response) {
+
+      $scope.users = response.data;
+      console.log(response.data);
+
+    }, function errorCallback(response) {
+      console.log("error to get user, maybe not login");
+    })
+  };
+
+
+  $scope.deleteUser = function(userId){
+    $http({
+      method: 'DELETE',
+      url: '../api/users/' + userId
+    }).then(function successCallback(response) {
+      getUsers();
+
+      console.log(response);
+
+    }, function errorCallback(response) {
+      console.log(response);
+    })
+  }
+
+  $scope.addTeacher = function(userId){
+    $http.post('../api/teacher/' + userId).
+    success(function(data) {
+      $rootScope.$broadcast('refreshTeacherList');
+      getUsers();
+      console.log("posted successfully");
+    }).error(function(data) {
+      console.error("error in posting");
+    })
+  }
+
+  $scope.$on('refreshUserList', function(event, args) {
+    getUsers();
+  });
+
+
+})
+
+  .controller('manageStudents', function($rootScope, $scope, $http) {
+  getStudents();
+
+  function getStudents(){
+    $http({
+      method: 'GET',
+      url: '../api/students'
+    }).then(function successCallback(response) {
+
+      $scope.students = response.data;
+      console.log(response.data);
+
+    }, function errorCallback(response) {
+      console.log("error to get user, maybe not login");
+    });
+
+  };
+
+  $scope.deleteStudent = function(studentId){
+    $http({
+      method: 'DELETE',
+      url: '../api/student/' + studentId
+    }).then(function successCallback(response) {
+      getStudents();
+      $rootScope.$broadcast('refreshUserList');
+
+      console.log(response);
+
+    }, function errorCallback(response) {
+      console.log(response);
+    })
+  }
+
+  $scope.createStudents = function(){
+    var data = JSON.parse($scope.schoolids);
+    console.log(data);
+    $http({
+      url: '../api/students/multi',
+      method: "POST",
+      data: $.param({data}),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    })
+      .then(function(response) {
+      // successconso
+      console.log(response);
+    },
+            function(response) { // optional
+      // failed
+    });
+  }
+
+})
+
+  .directive('ngReallyClick', [function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element.bind('click', function() {
+          var message = attrs.ngReallyMessage;
+          if (message && confirm(message)) {
+            scope.$apply(attrs.ngReallyClick);
+          }
+        });
+      }
+    }
+  }]);
---
+++ client/public/js/ng-app-home.js
@@ -0,0 +1,330 @@
+angular.module('home', ['ngMaterial','ngMessages'])
+  .config(function($interpolateProvider, $httpProvider){
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+  //Reset headers to avoid OPTIONS request (aka preflight)
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+})
+
+  .controller('homeController', function($rootScope, $scope, $http) {
+
+  console.log("hello home!")
+
+})
+  .controller('homeworkManageController', function($rootScope, $scope, $http, $mdDialog, $mdMedia) {
+
+  $scope.showNewHomeworkForm = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+    $mdDialog.show({
+      controller: NewHWDialogController,
+      templateUrl: '/html/newhomeworkform.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:false,
+      fullscreen: useFullScreen
+    })
+      .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+
+  };
+
+
+
+})
+
+  .controller('groupsManageController', function($rootScope, $scope, $http, $mdDialog, $mdMedia) {
+  getMyGroups();
+  $scope.myGroups = [];
+
+  $scope.bTt = function(b){
+    if(b === true){
+      return "公開小組";
+    }else{
+      return "私人小組";
+    }
+  }
+  var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+  $scope.showNewGroupForm = function(ev) {
+
+    $mdDialog.show({
+      templateUrl: '/html/newgroupform.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:false,
+      fullscreen: useFullScreen,
+      controllerAs: 'ctrl',
+      controller: "NewGDialogController"
+    })        .then(function(refreshMyGroups) {
+      if(refreshMyGroups == "success"){
+        getMyGroups();
+      }
+    }, function() {
+      console.log('You cancelled the dialog.');
+    });
+  };
+
+  $scope.showGroupDialog = function(ev, g) {
+    $scope.group = g;
+    $mdDialog.show({
+      templateUrl: '/html/groupmodal.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen,
+      locals: {
+        group: $scope.group
+      },
+      controller: "GroupModalController"
+    });
+
+  };
+
+  function getMyGroups(){
+    $http({
+      url: '/api/teacher/groups/fromtc',
+      method: "GET",
+    })
+      .then(function(response) {
+      $scope.myGroups = response.data.teachGroups;
+      //       console.log("GET TEACHER'S GROUP SUCCESS");
+    },
+            function(response) { // optional
+      console.log("fail to get teacher's groups")
+    });
+  }
+
+})
+
+  .controller('NewHWDialogController', function($rootScope, $scope, $http, $mdDialog, $mdMedia) {
+  $scope.subjects = ["數學","中文","英文","通識"]
+
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+})
+
+  .controller('NewGDialogController', function($scope, $http, $mdDialog, $mdMedia, $timeout, $interval) {
+
+  $scope.newgroup = {
+    public: {
+      boolean : "true"
+    }
+  };
+
+  $scope.searchDone = true;
+
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  var cs = [];
+  var searchedText = [];
+  var typingTimer;                //timer identifier
+  var doneTypingInterval = 1000;
+  //on keyup, start the countdown
+  $scope.keyup = function(){
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  }
+  //on keydown, clear the countdown
+  $scope.keydown = function(){
+    if (event.keyCode !== 8) {
+      $scope.searchDone = false;
+    }
+    clearTimeout(typingTimer);
+  }
+
+  //user is "finished typing," do query
+  function doneTyping () {
+    if((self.searchText !== '') && !(arrayContains(self.searchText, searchedText))){
+      queryStudents(self.searchText);
+    }
+  }
+
+  function queryStudents(query){
+    $http({
+      method: 'GET',
+      url: '/api/students/query/' + query
+    }).then(function successCallback(response) {
+      for (var i = 0; i < response.data.length; i++) {
+        var chipObject =
+            {
+              "schoolid": response.data[i]["schoolId"],
+              "name": response.data[i]["name"],
+              "id": response.data[i]["_id"]
+            };
+
+        if(!containsObject(chipObject, cs)){
+          cs.unshift(chipObject);
+        }
+
+        if(!arrayContains(response.data[i]["name"], searchedText)){
+          searchedText.push(response.data[i]["name"]);
+        }
+
+        if(!arrayContains(response.data[i]["schoolid"], searchedText)){
+          searchedText.push(response.data[i]["schoolId"]);
+        }
+      }
+
+      if(response.data.length !== 0){
+        if(!arrayContains(query, searchedText)){
+          searchedText.push(query);
+        }
+      }
+      $scope.searchDone = true;
+      self.vegetables = loadVegetables();
+    }, function errorCallback(response) {
+      console.log(response);
+    })
+
+  }
+
+  function containsObject(obj, list) {
+    for (i = 0; i < list.length; i++) {
+      if (list[i]["schoolid"] == obj["schoolid"]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function arrayContains(needle, arrhaystack)
+  {
+    return (arrhaystack.indexOf(needle) > -1);
+  }
+
+  var self = this;
+
+  self.readonly = false;
+  self.selectedItem = null;
+  self.searchText = null;
+  self.querySearch = querySearch;
+  self.selectedVegetables = [];
+  self.numberChips = [];
+  self.numberChips2 = [];
+  self.numberBuffer = '';
+  self.autocompleteDemoRequireMatch = true;
+  self.transformChip = transformChip;
+
+  /**
+           * Return the proper object when the append is called.
+           */
+  function transformChip(chip) {
+    // If it is an object, it's already a known chip
+    if (angular.isObject(chip)) {
+      return chip;
+    }
+    // Otherwise, create a new one
+    return {
+      name: chip,
+      type: 'new'
+    }
+  }
+
+  /**
+           * Search for vegetables.
+           */
+  function querySearch(query) {
+    if(cs.length !== 0){
+      var results = query ? self.vegetables.filter(createFilterFor(query)) : [];
+      return results;
+    }
+  }
+
+  /**
+           * Create filter function for a query string
+           */
+  function createFilterFor(query) {
+
+    console.log(query);
+    //     var lowercaseQuery = angular.lowercase(query);
+    return function filterFn(vegetable) {
+      return (vegetable.name.indexOf(query) != -1) ||
+        (vegetable.schoolid.indexOf(query) != -1);
+    };
+
+  }
+
+  function loadVegetables() {
+    var veggies = cs;
+    //     return veggies.map(function(veg) {
+    //       veg._lowername = veg.name.toLowerCase();
+    //       veg._lowertype = veg.type.toLowerCase();
+    //       return veg;
+    //     });
+    return veggies;
+  }
+
+  $scope.createGroup = function(){
+    var data = {
+      "name" : $scope.newgroup.name,
+      "public" : $scope.newgroup.public.boolean,
+      "students" : self.selectedVegetables
+    };
+
+    console.log(data);
+    $http({
+      url: '/api/group',
+      method: "POST",
+      data: data,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    })
+      .then(function(response) {
+      // success
+      $mdDialog.hide("success");
+    },
+            function(response) { // optional
+      // failed
+    });
+  }
+})
+
+  .controller('GroupModalController', function($scope, $http, $mdDialog, $mdMedia, group) {
+  getGroupDetails();
+  $scope.group = group;
+
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  function getGroupDetails(){
+    $http({
+      url: '/api/teacher/group/' + group._id,
+      method: "GET",
+    })
+      .then(function(response) {
+      $scope.gDetails = response.data;
+      //       console.log("get group's details success");
+    },
+            function(response) { // optional
+      console.log("fail to get group's details")
+    });
+  }
+})
+
+
+
+
+
+
+
